7366575488abb206949012c765a611e630283722
Manjit Singh <manjitcse@gmail.com>
2020-05-30 18:16:08 +0530

    added git and homepage menu-item

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage.componet'
+import HomePage from './homepage.componet';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
