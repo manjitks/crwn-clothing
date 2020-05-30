552e966d21e58c374dcd153d5753aaad93d7077a
Manjit Singh <manjitcse@gmail.com>
2020-05-30 22:46:15 +0530

    menu-item componet creted and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './homepage.componet'
-import HomePage from './homepage.componet';
+import Homepage from './pages/homepage/homepage.componet'
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Homepage/>
     </div>
   );
 }
