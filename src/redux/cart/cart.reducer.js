449ecab6ec775ed9a0f52edef7c6d2871c5e35af
Manjit Singh <manjitcse@gmail.com>
2020-06-06 00:45:51 +0530

    added clear items and quantity in checkoutpage

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.type';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart,removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,16 @@ const INITIAL_STATE = {
           ...state,
           cartItems: addItemToCart(state.cartItems, action.payload)
         };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+              ...state,
+              cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+      case CartActionTypes.CLEAR_CART_ITEM:
+      return{
+          ...state,
+          cartItems:state.cartItems.filter((c) => c.id !== action.payload.id)
+      }
       default:
         return state;
     }
