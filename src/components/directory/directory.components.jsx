72f10ceaafd3d684ff923161f4b2f03c30f1be23
Manjit Singh <manjitcse@gmail.com>
2020-06-05 17:51:53 +0530

    action type variblcreated and redirect for authenticated user

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -49,7 +49,7 @@ class Directory extends React.Component {
 
             <div className="directory-menu">
                 {this.state.sections.map(({id,...section}) => (
-                    <MenuItem id={id} {...section}  />
+                    <MenuItem key={id} {...section}  />
                 ))}
             </div>
 
